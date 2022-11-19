60b91d71465f1ca5e729d514e21b259f42af73b8
LAPTOP-RQBHQ9EN/Kent Angelo <cabanagkentangelo@gmail.com>
2022-11-19 12:10:57 +0800

    Adding service provider

--- Tracking_Phone.py
+++ Tracking_Phone.py
@@ -8,3 +8,14 @@ TheNumber = phonenumbers.parse(number)
 
 yourLocation = geocoder.description_for_number(TheNumber, "en")
 print(yourLocation)
+
+
+# get service provider
+from phonenumbers import carrier
+
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
+
+
+
+#CTTO https://www.youtube.com/watch?v=Geisa_ib5hs&t=506s
